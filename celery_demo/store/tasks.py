2eca9047f24693d738c0bae475d0fe0a53db42e4
Derek Stegelman <dstegelman@gmail.com>
2014-06-27 22:10:01 -0600

    Add another task.

--- celery_demo/store/tasks.py
+++ celery_demo/store/tasks.py
@@ -1,5 +1,5 @@
 from __future__ import absolute_import
-
+import time
 from celery import shared_task
 
 
@@ -7,3 +7,10 @@ from celery import shared_task
 def add(x, y):
     print(x + y)
     return x + y
+
+
+@shared_task
+def process_payment():
+	# this takes a long time to process
+	time.sleep(10)
+	print("processed")
