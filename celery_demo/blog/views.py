c03ebbe073ca89f876749e606d66e1dbf22c248a
Derek Stegelman <dstegelman@gmail.com>
2014-08-11 11:06:59 -0600

    update.

--- celery_demo/blog/views.py
+++ celery_demo/blog/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+
+def blocking_view(request, object_pk=None):
+    object = get_object_or_404(SomeObject, pk=object_pk)
+    object.do_method()
+
+    # Non-blocking.  Celery takes over this code and lets us move right along
+    ObjectTransaction.delay(object)
+
+    return render(request, 'template.html')
